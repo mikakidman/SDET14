451e834e1f7798c7c20e9448b1c8b96e7c5fa919
mikakidman <kitmanovamz@gmail.com>
2022-10-29 22:43:58 -0400

    ArrayListDemo2

---
+++ src/Class24/ArrayListDemo3.java
@@ -0,0 +1,31 @@
+package Class24;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo3 {
+    public static void main(String[] args) {
+        ArrayList<Character> chars=new ArrayList<>();
+        chars.add('A');
+        chars.add('B');
+        chars.add('C');
+        chars.add('D');
+        chars.add('E');
+        chars.add('F');
+        System.out.println(chars);
+
+        ArrayList<Boolean>booleans=new ArrayList<>();
+        booleans.add(false);
+        booleans.add(false);
+        booleans.add(true);
+        System.out.println(booleans);
+
+        ArrayList<Float>floats=new ArrayList<>();
+        floats.add(12.5f);
+        floats.add(13f);
+        floats.add(15f);
+        System.out.println(floats);
+
+
+    }
+}
+
